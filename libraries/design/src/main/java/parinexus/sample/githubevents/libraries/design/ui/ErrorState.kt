1be487564ddf3e3403ddf528e2c36d96a01dab04
Parinexus <Parinexus@gmail.com>
2025-09-07 18:32:52 +0330

    Implement error state view with its preview

---
+++ libraries/design/src/main/java/parinexus/sample/githubevents/libraries/design/ui/ErrorState.kt
@@ -0,0 +1,73 @@
+package parinexus.sample.githubevents.libraries.design.ui
+
+import parinexus.sample.githubevents.libraries.design.R
+import parinexus.sample.githubevents.libraries.design.extensions.clickableWithNoRipple
+import parinexus.sample.githubevents.libraries.design.theme.GitHubEventsTheme
+import parinexus.sample.githubevents.libraries.design.theme.HintColor
+import parinexus.sample.githubevents.libraries.design.theme.Red
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ErrorState(modifier: Modifier = Modifier, onRetry: () -> Unit = {}) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .clickableWithNoRipple { onRetry() }
+            .testTag("errorState"),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_github),
+            contentDescription = "errorStatePlaceHolderImage",
+            colorFilter = ColorFilter.tint(MaterialTheme.colors.surface)
+        )
+        Text(
+            modifier = Modifier.padding(top = 16.dp),
+            text = stringResource(R.string.error),
+            fontSize = 12.sp,
+            color = Red,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            modifier = Modifier.padding(top = 8.dp),
+            text = stringResource(R.string.errorText),
+            fontSize = 12.sp,
+            color = HintColor
+        )
+        Text(
+            modifier = Modifier.padding(top = 8.dp),
+            text = stringResource(R.string.tapToRetry),
+            fontSize = 12.sp,
+            color = HintColor
+        )
+    }
+}
+
+@Preview
+@Composable
+fun ErrorStatePreview() {
+    GitHubEventsTheme {
+        Surface {
+            ErrorState()
+        }
+    }
+}
