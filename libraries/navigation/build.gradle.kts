40fbabd47569b58d8bb324bd3c05485411040fb2
Parinexus <Parinexus@gmail.com>
2025-09-07 18:26:24 +0330

    Add base files of navigation module

---
+++ libraries/navigation/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("githubevents.android.library")
+}
+
+android {
+    namespace = "parinexus.sample.githubevents.libraries.navigation"
+    buildFeatures {
+        buildConfig = true
+    }
+}
