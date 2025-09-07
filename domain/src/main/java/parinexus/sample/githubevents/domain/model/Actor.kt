2d8b1d1bd00c8a748d6b6f5391099742625e0914
Parinexus <Parinexus@gmail.com>
2025-09-07 18:20:10 +0330

    Update Actor and Event models, add RepoModel
    - Remove `id` from `Actor` model.
    - Add `type` to `Event` model and change `repo` type to `RepoModel`.
    - Create `RepoModel` with `name` and `url` fields.

--- domain/src/main/java/parinexus/sample/githubevents/domain/model/Actor.kt
+++ domain/src/main/java/parinexus/sample/githubevents/domain/model/Actor.kt
@@ -1,7 +1,6 @@
 package parinexus.sample.githubevents.domain.model
 
 data class Actor(
-    val id: Long,
     val login: String,
     val avatarUrl: String
 )
--- domain/src/main/java/parinexus/sample/githubevents/domain/model/GithubRepository.kt
+++
@@ -1,7 +0,0 @@
-package parinexus.sample.githubevents.domain.model
-
-data class GithubRepository(
-    val id: Long,
-    val name: String,
-    val url: String
-)
